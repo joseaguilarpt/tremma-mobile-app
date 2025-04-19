dbb97311ba5c6ddcd7cbd152aa861375a9b67985
Jose Aguilar <joseaguilarpt@gmail.com>
2025-04-19 16:51:30 +0100

    Fix general issues and improve stability

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,5 +1,5 @@
 import { useAuth } from "@/context/auth";
-import { useFocusEffect, useNavigationState } from "@react-navigation/native";
+import { useFocusEffect } from "@react-navigation/native";
 import React from "react";
 
 export default function ProtectedRoute({
